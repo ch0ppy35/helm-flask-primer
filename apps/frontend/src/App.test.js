3a58c15d99d0dd7553d9545470b1420fc3f09670
Mike Miller <chop3593@gmail.com>
2021-09-29 23:08:34 -0600

    clean up

--- apps/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders My K8s Lab', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/K8s Lab/i);
-  expect(linkElement).toBeInTheDocument();
-});
